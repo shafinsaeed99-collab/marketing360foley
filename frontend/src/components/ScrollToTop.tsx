0fec39c5db9acd9e51f286fd506c3c35bf92c9ef
Taha Rehman <taharehman419@gmail.com>
2026-07-09 01:52:12 +0500

    fixed scroll to top error

--- frontend/src/components/ScrollToTop.tsx
+++ frontend/src/components/ScrollToTop.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react"
 import { useLocation } from "react-router-dom"
 
-export default function ScrollToTop() {
+export function ScrollToTop() {
   const { pathname, hash } = useLocation()
 
   useEffect(() => {
@@ -17,5 +17,5 @@ export default function ScrollToTop() {
     window.scrollTo(0, 0)
   }, [pathname, hash])
 
-  return null
+  return <></>
 }
